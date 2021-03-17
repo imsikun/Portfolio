0f80bb2f11bf12858d5195563c24e54f5bea838e
Subh <subhakant@staragrifinance.com>
2021-03-17 13:53:57 +0530

    experience done

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,12 +6,12 @@
 
 module.exports = {
   siteMetadata: {
-    title: "WebDev Portfolio",
-    description: "This is WebDev Portfolio Site",
-    author: "@webdev",
-    twitterUsername: "@john_smilga",
-    image: "/twitter-img.png",
-    siteUrl: "https://testing-strapi-gatsby-build.netlify.app",
+    title: 'WebDev Portfolio',
+    description: 'This is WebDev Portfolio Site',
+    author: '@webdev',
+    twitterUsername: '@john_smilga',
+    image: '/twitter-img.png',
+    siteUrl: 'https://testing-strapi-gatsby-build.netlify.app'
   },
   plugins: [
     `gatsby-transformer-sharp`,
@@ -22,8 +22,8 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `assets`,
-        path: `${__dirname}/src/assets/`,
-      },
+        path: `${__dirname}/src/assets/`
+      }
     },
     {
       resolve: `gatsby-source-strapi`,
@@ -33,10 +33,10 @@ module.exports = {
         //   contentTypes : `jobs`, `projects`, `blogs`,
         //   singleType : `about`
         //  ONLY ADD TO ARRAY IF YOU HAVE DATA IN STRAPI !!!!
-        contentTypes: [],
-        singleTypes: [],
-      },
-    },
+        contentTypes: [`jobs`],
+        singleTypes: []
+      }
+    }
     // {
     //   resolve: `gatsby-plugin-webfonts`,
     //   options: {
@@ -51,5 +51,5 @@ module.exports = {
     //     },
     //   },
     // },
-  ],
+  ]
 }
